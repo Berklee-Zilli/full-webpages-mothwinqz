cbf9f2f81280fd2cc2aadb8aa5e0bda724d08c4c
luca willoughby <sol@meatball.local>
2024-11-19 19:17:56 -0500

    level 1 completegit add task1

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,9 @@
+document.title = "Task 1 by luca hooray"
+
+document.querySelector("h1").innerHTML = "TASK 1"
+
+document.getElementById("intro").innerHTML = "my height changes slightly at each doctor's appointment and i don't know why"
+
+document.querySelector("h2").innerHTML = "list of things i like!"
+
+document.getElementByClassName("list-item").innerHTML = "sad test"
